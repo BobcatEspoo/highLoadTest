4b6966c2a7a0c306661ecbe003dc01f9f68b3271
BobcatEspoo <iliapronovich@gmail.com>
2025-09-11 10:28:17 +0300

    try: run without recorder

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ sudo apt install sshpass
 sudo apt install ffmpeg
 cd test_run
 
-chmod +x recorder
+# chmod +x recorder
 chmod +x my_linux_app
 
 ./start_game
 
-./recorder -name=$1
+# ./recorder -name=$1
 
 bash saveRecord.sh
