77f932778bfe7bb58f78771fb49c2da31246b5c6
BobcatEspoo <iliapronovich@gmail.com>
2025-09-11 13:13:53 +0300

    fix: add create of x screen

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 sudo apt install sshpass
-sudo apt install ffmpeg
+sudo apt install -y ffmpeg
+Xvfb :99 -screen 0 1920x1080x24 &
+export DISPLAY=:99
 cd test_run
 
-# chmod +x recorder
+chmod +x recorder
 chmod +x my_linux_app
 
 ./start_game
 
-# ./recorder -name=$1
+./recorder -name=$1
 
 bash saveRecord.sh
