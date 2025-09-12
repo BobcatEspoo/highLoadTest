ebb4c4381dbe23bc503ede36579762ab5c6a8914
BobcatEspoo <iliapronovich@gmail.com>
2025-09-12 15:11:38 +0300

    change game

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// chromePath := `C:\Program Files\Google\Chrome\Application\chrome.exe`
 	chromeLinuxPath := "/usr/local/bin/google-chrome"
-	url := "https://x.la/cgs/empire-of-the-ants/play"
+	url := "https://x.la/cgs/1754888695/play"
 	args := []string{
 		"--password-store=basic",
 		"--no-first-run",
