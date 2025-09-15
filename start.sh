7ef30dac200b18b62edca14274851d73ce522605
Andrey Kryuchenko <andrey@example.com>
2025-09-15 11:41:36 +0200

    Fix start.sh to use Linux binary and install Chrome properly

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-sudo apt install sshpass
+sudo apt install -y google-chrome-stable
 # sudo apt install xvfb ffmpeg -y
 # Xvfb :99 -screen 0 1920x1080x24 &
 # export DISPLAY=:99
 # DISPLAY=:99 xclock
-cd test_run
 
-# chmod +x recorder
-chmod +x ./start_game
-file ./start_game
-uname -m
-./start_game
+# Use the Linux binary we built
+chmod +x ./highLoadTest
+./highLoadTest
 
 # ./recorder -name=$1
 
